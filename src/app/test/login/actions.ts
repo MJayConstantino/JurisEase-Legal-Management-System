bbc2aef85b7f3678b58ae4358c29298fe4f56793
A. Legayada <tonilegayada@gmail.com>
2025-03-08 03:29:25 +0800

    zod update

--- src/app/test/login/actions.ts
+++ src/app/test/login/actions.ts
@@ -4,15 +4,27 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
+import { z } from 'zod'
+
+const userSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5),
+})
+
+export type User = z.infer<typeof userSchema>
 
 export async function login(formData: FormData) {
   const supabase = await createClient()
   //{{ .SiteURL }}/auth/confirm?token_hash={{ .TokenHash }}&type=email. change configuration settings soon
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
+
+  // const data: User = {
+  //   email: formData.get('email') as string,
+  //   password: formData.get('password') as string,
+  // } this is the old code by supabase to be used a reference
+
+  const data = Object.fromEntries(formData.entries()) as User
+  if (userSchema.safeParse(data).error) {
+    redirect('test/login?message= Invalid credentials')
   }
 
   const { error } = await supabase.auth.signInWithPassword(data)
