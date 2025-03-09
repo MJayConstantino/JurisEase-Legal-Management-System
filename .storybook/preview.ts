8ef9d5f676b407d4defdf9fdabde41c1253eae23
A. Legayada <tonilegayada@gmail.com>
2025-03-09 23:53:00 +0800

    enabled dark mode on sb

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,15 @@
 import type { Preview } from '@storybook/react'
-
+import { themes } from '@storybook/theming'
 const preview: Preview = {
   parameters: {
+    docs: { theme: themes.dark },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
