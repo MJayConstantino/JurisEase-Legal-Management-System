d89fb394119dda451c591e804a28450f82547e27
A. Legayada <tonilegayada@gmail.com>
2025-03-15 01:32:54 +0800

    implement sign out button test

--- src/components/header/signout.tsx
+++ src/components/header/signout.tsx
@@ -5,7 +5,7 @@ import { useTransition } from 'react'
 interface SignoutProps {
   signOutfn?: () => Promise<void>
 }
-export function Signout({ signOutfn = handleSignOut }) {
+export function Signout({ signOutfn = handleSignOut }: SignoutProps) {
   const [isPending, startTransition] = useTransition()
   const onSignOut = async () => {
     startTransition(async () => {
