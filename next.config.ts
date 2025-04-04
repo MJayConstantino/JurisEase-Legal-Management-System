c293e12a386ede018d5f71d3855a1dba92399bfd
A. Legayada <tonilegayada@gmail.com>
2025-04-04 19:58:52 +0800

    added test tsconfig
    - since i don't want the testing to affect the production settings

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    dirs: [
-      "src/app",
-      "src/components"
-    ],
+    dirs: ['src/app', 'src/components'],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
