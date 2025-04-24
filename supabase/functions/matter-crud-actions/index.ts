69489748c30f5af795f63ac4ac06dad9ac80bf47
MJayConstantino <mjconstantino12345@gmail.com>
2025-04-24 10:22:48 +0800

    chore: deployed matter-crud-actions in supabase edge functions

---
+++ supabase/functions/matter-crud-actions/index.ts
@@ -0,0 +1,20 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
+
+const supUrl = Deno.env.get("_SUPABASE_URL") as string;
+const supKey = Deno.env.get("_SUPABASE_SERVICE_KEY") as string;
+const supabase = createClient(supUrl, supKey);
+
+console.log("Hello from Functions!");
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/matter-crud-actions' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
