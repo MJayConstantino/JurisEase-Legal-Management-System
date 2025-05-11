52fe3ec45383a620d15f10038b8427ff29529462
MJayConstantino <mjconstantino12345@gmail.com>
2025-05-12 01:50:09 +0800

    fix: lint issues

--- src/components/matters/dashboardCards/caseDetailsCard.tsx
+++ src/components/matters/dashboardCards/caseDetailsCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { EditableCard } from "../editableCard";
 import type { Matter } from "@/types/matter.type";
 import type { User as UserType } from "@/types/user.type";
