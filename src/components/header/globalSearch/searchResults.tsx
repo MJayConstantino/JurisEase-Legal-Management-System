dea7719fd86941ca251c237327b0b3a56fe08196
A. Legayada <tonilegayada@gmail.com>
2025-05-15 12:24:08 +0800

    changed to switch brach

--- src/components/header/globalSearch/searchResults.tsx
+++ src/components/header/globalSearch/searchResults.tsx
@@ -108,7 +108,6 @@ export function SearchResults({
         case 'paid':
           return 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300'
         case 'overdue':
-        case 'unpaid':
           return 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300'
         default:
           return 'bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-300'
