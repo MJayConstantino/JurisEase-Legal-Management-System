f53de6a1f595f062ad2410c56e2c7d0d1b606325
A. Legayada <tonilegayada@gmail.com>
2025-05-17 01:57:52 +0800

    Update globalSearch.cy.ts

--- cypress/e2e/globalSearch.cy.ts
+++ cypress/e2e/globalSearch.cy.ts
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Global Search E2E', () => {
+  before(() => {
+    cy.signUp('Test User', 'test@testdomain.com', 'testPassword')
+    cy.task('confirmUserEmail', 'test@testdomain.com')
   })
+  beforeEach(() => {
+    cy.login('test@testdomain.com', 'testPassword')
+    cy.wait(5000)
+    cy.visit('/matters').wait(5000)
+  })
+  describe('When the user searches for a matter', () => {})
+
+  describe('When the user searches for a task ', () => {})
+
+  describe('When the user searches for a bill', () => {})
 })
