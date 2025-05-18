78c27f63c7acaf82a067a19d6e98a788105befe9
A. Legayada <tonilegayada@gmail.com>
2025-05-19 00:14:27 +0800

    Update globalSearch.cy.ts

--- cypress/e2e/globalSearch.cy.ts
+++ cypress/e2e/globalSearch.cy.ts
@@ -7,14 +7,6 @@ describe('Global Search E2E', () => {
   beforeEach(() => {
     cy.login('test@testdomain.com', 'testPassword')
     cy.wait(5000)
-    cy.visit('/matters').wait(5000)
-    cy.get('.border-input').click()
-    cy.get('#clientName').click()
-    cy.get('#attorney').click()
-    cy.get('#caseName').click()
-    cy.get('#matters').click()
-    cy.get('#tasks').click()
-    cy.get('#bills').click()
   })
   after(() => {
     cy.cleanUpSearchTests()
@@ -22,6 +14,16 @@ describe('Global Search E2E', () => {
 
   // MATTERS SECTION
   describe('When the user wants to search for a matter', () => {
+    beforeEach(() => {
+      cy.visit('/matters').wait(5000)
+      cy.get('.border-input').click()
+      cy.get('#clientName').click()
+      cy.get('#attorney').click()
+      cy.get('#caseName').click()
+      cy.get('#matters').click()
+      cy.get('#tasks').click()
+      cy.get('#bills').click()
+    })
     it('should not yield results if the matter checkbox is unchecked', () => {
       cy.get('input[aria-label="DialogBoxSearch"]').type('NoMatterResult')
       cy.get('input[aria-label="DialogBoxSearch"]').type('testMatter')
@@ -35,7 +37,7 @@ describe('Global Search E2E', () => {
         .contains(/testMatter/i)
         .should('be.visible')
     })
-    it('shoould display relevant matters whose nameor client matches the search query if the matter checkbox is checked and the client search by checkbox is checked', () => {
+    it('shoould display relevant matters whose name or client matches the search query if the matter checkbox is checked and the client search by checkbox is checked', () => {
       cy.get('#matters').click()
       cy.get('#clientName').click()
       cy.get('input[aria-label="DialogBoxSearch"]').type('testMatterClient')
@@ -78,9 +80,72 @@ describe('Global Search E2E', () => {
   })
 
   //TASKS SECTION
-  // describe('When the user wants to search for a task', () => {
+  describe.skip('When a user wants to search for a task', () => {
+    beforeEach(() => {
+      cy.visit('/tasks').wait(5000)
+      cy.get('.border-input').click()
+      cy.get('#clientName').click()
+      cy.get('#attorney').click()
+      cy.get('#caseName').click()
+      cy.get('#matters').click()
+      cy.get('#tasks').click()
+      cy.get('#bills').click()
+    })
 
-  // })
+    it('should not yield results if the matter checkbox is unchecked', () => {
+      cy.get('input[aria-label="DialogBoxSearch"]').type('Mock Task')
+      cy.contains(/no results/i).should('be.visible')
+    })
+    it('shoould display relevant tasks whose name (including those without matters) matches the search query if the tasks checkbox is checked and no search by checkboxes are checked', () => {
+      cy.get('#matters').click()
+      cy.get('input[aria-label="DialogBoxSearch"]').type('Mock Task')
+      cy.wait(2000)
+        .contains(/Mock Task (No Matter)/i)
+        .should('be.visible')
+      cy.contains(/Mock Task (With Matter)/i).should('be.visible')
+    })
+
+    it('shoould display relevant matters whose name or client matches the search query if the matter checkbox is checked and the client search by checkbox is checked', () => {
+      cy.get('#matters').click()
+      cy.get('#clientName').click()
+      cy.get('input[aria-label="DialogBoxSearch"]').type('testMatterClient')
+      cy.wait(2000)
+        .contains(/testMatter/i)
+        .should('be.visible')
+    })
+    it('shoould display relevant matters whose name or assigned attorney matches the search query if the matter checkbox is checked and the attorney search by checkbox is checked', () => {
+      cy.get('#matters').click()
+      cy.get('#attorney').click()
+      cy.get('input[aria-label="DialogBoxSearch"]').type('testUser')
+      cy.wait(2000)
+        .contains(/testMatter/i)
+        .should('be.visible')
+    })
+    it('shoould display relevant matters whose name matches the search query if the matter checkbox is checked and the case name search by checkbox is checked', () => {
+      cy.get('#matters').click()
+      cy.get('#caseName').click()
+      cy.get('input[aria-label="DialogBoxSearch"]').type('testMatter')
+      cy.wait(2000)
+        .contains(/testMatter/i)
+        .should('be.visible')
+    })
+    it('shoould display relevant matters whose name or opposing council name matches the search query if the matter checkbox is checked and the opposing council search by checkbox is checked', () => {
+      cy.get('#matters').click()
+      cy.get('#opposingCouncil').click()
+      cy.get('input[aria-label="DialogBoxSearch"]').type('testOpposingCouncil')
+      cy.wait(2000)
+        .contains(/testMatter/i)
+        .should('be.visible')
+    })
+    it('shoould display relevant matters whose name or court name matches the search query if the matter checkbox is checked and the court search by checkbox is checked', () => {
+      cy.get('#matters').click()
+      cy.get('#court').click()
+      cy.get('input[aria-label="DialogBoxSearch"]').type('testCourt')
+      cy.wait(2000)
+        .contains(/testMatter/i)
+        .should('be.visible')
+    })
+  })
   // describe('When the user searches for a task ', () => {})
 
   // describe('When the user searches for a bill', () => {})
