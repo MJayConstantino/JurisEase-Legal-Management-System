33a9bf0e6f0af611353b3d773a28ca6d880ae345
A. Legayada <tonilegayada@gmail.com>
2025-05-21 21:20:08 +0800

    added skeleton for error fallback

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import ErrorPage from '@/components/auth/ErrorPage'
+import { ErrorPageSkeleton } from '@/components/auth/ErrorPageSkeleton'
+import { Suspense } from 'react'
 
 export default function Error() {
   return (
     <>
-      <ErrorPage />
+      <Suspense fallback={<ErrorPageSkeleton />}>
+        <ErrorPage />
+      </Suspense>
     </>
   )
 }
